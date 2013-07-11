895d2bc34526012899f6fa8b092beb77e16a6d8a
Jesse Appel <jesseappel@gmail.com>
2013-07-11 22:35:33 +0000

    Added web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 4!');
+var hello_world=buf.toString(fs.readFileSync('index.html'));
+
+
+  response.send(hello_world);
 });
 
 var port = process.env.PORT || 5000;
